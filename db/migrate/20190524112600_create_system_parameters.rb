d9d7f3ff9102b77cf3591e2368d6afdfb8c27bd0
Giuliano Crivelli <giuliano@agenciaw3.digital>
2019-05-24 09:24:31 -0400

    adding new param

--- db/migrate/20190524112600_create_system_parameters.rb
+++ db/migrate/20190524112600_create_system_parameters.rb
@@ -3,17 +3,24 @@ class CreateSystemParameters < ActiveRecord::Migration[5.2]
     create_table :system_parameters do |t|
 
       t.boolean :survey
+
       t.boolean :photo_gallery
       t.boolean :photo_gallery_home
+
       t.boolean :video_gallery
       t.boolean :video_gallery_home
+
       t.boolean :nav_menu
       t.boolean :nav_menu_top
       t.boolean :nav_menu_side
       t.boolean :nav_menu_side_left
+
       t.boolean :banner
+
       t.boolean :big_slide
+
       t.boolean :last_news
+      
       t.boolean :featured
 
       t.timestamps

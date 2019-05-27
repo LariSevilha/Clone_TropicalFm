a569feb7850387422a69c70b090e80715bd0acee
ericklino <e7.ifms@gmail.com>
2019-05-27 08:15:04 -0400

    adjust skeleton

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
   has_many :link
 
-  enum type_menu: {top: 0, side: 1 }
-  enum position_menu: {left: 0, right: 1 }
+  enum type_menu: {topo: 0, lateral: 1 }
+  enum position_menu: {esquerda: 0, direita: 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.new({
-  :name => "Agência W3",
-  :email => "suporte@agenciaw3.digital",
-  :password => "w3case2022",
-  :password_confirmation => "w3case2022",
-  :admin => true
+# User.new({
+#   :name => "Agência W3",
+#   :email => "suporte@agenciaw3.digital",
+#   :password => "w3case2022",
+#   :password_confirmation => "w3case2022",
+#   :admin => true
+# }).save
+
+SystemParameter.new({
+  :survey => false,
+  :photo_gallery => false,
+  :photo_gallery_home => false,
+  :video_gallery => false,
+  :video_gallery_home=> false,
+  :nav_menu => false,
+  :banner=> false,
+  :big_slide => false,
+  :news => false,
+  :last_news => false,
+  :featured => false
 }).save

2c82495843399cdc4fa3b98a36c3c1ec8943dc0e
Giuliano Crivelli <giuliano@agenciaw3.digital>
2020-06-24 11:05:37 -0400

    fix uploader content builder

--- app/uploaders/content_builder_image_uploader.rb
+++ app/uploaders/content_builder_image_uploader.rb
@@ -5,7 +5,11 @@ class ContentBuilderImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :sftp
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.

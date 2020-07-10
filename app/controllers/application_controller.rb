56e5e28993ef921f34a3e66144d5aa63daafda6b
Giuliano Crivelli <giuliano@agenciaw3.digital>
2020-07-10 08:25:31 -0400

    added keyword on controller, fixed meta tags and triple title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  require "mini_magick"
+
   protect_from_forgery with: :exception
 
   helper_method :logo
@@ -12,6 +14,14 @@ class ApplicationController < ActionController::Base
       # Padding: 160px
       @page_image = request.base_url + 
       ActionController::Base.helpers.asset_path('icon.png')
+    end
+
+    if !Rails.env.development?
+      image_information = MiniMagick::Image.open(@page_image.to_s)
+
+      @page_image_width = image_information[:width]
+      @page_image_height = image_information[:height]
+      @page_image_type = image_information[:format].downcase
     end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @page_title = "Skeleton"
     @page_description = "Skeleton Description"
+    @page_keywords = ['Home', 'Início', 'Rails', 'Skeleton']
     logo
   end
 end

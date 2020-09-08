e11e77f5415fb07bf125944654902681a824ee62
Giuliano Crivelli <giuliano@agenciaw3.digital>
2020-09-08 13:26:18 -0400

    changed email to double quotes, added verification to logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,14 @@ class ApplicationController < ActionController::Base
     end
 
     if !Rails.env.development?
-      image_information = MiniMagick::Image.open(@page_image.to_s)
+      begin
+        image_information = MiniMagick::Image.open(@page_image.to_s)
+      rescue
+        @page_image = request.base_url + 
+        ActionController::Base.helpers.asset_path("logo-shared.jpg")
+
+        image_information = MiniMagick::Image.open(@page_image.to_s)
+      ensure
 
       @page_image_width = image_information[:width]
       @page_image_height = image_information[:height]
